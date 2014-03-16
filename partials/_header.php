2c925258881c871b9c0742fce753d7a7172e405c
totum <vallebo@gmail.com>
2014-03-16 13:36:51 -0700

    updating paths for ores

--- partials/_header.php
+++ partials/_header.php
@@ -1,14 +1,4 @@
-<!doctype html>
-<head>
-	<meta http-equiv="Content-Type" content="<?php bloginfo('html_type'); ?>; charset=<?php bloginfo('charset'); ?>" />
-	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
-	<title><?php wp_title('&laquo;', true, 'right'); ?> <?php bloginfo('name'); ?></title>
-	<link rel="stylesheet" type="text/css" href="<?php bloginfo('template_directory'); ?>/style.css" />
-	<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
-	<link href="/favicon.ico" rel="icon" type="image/x-icon" />
-	<?php wp_head(); ?>
-</head>
-
+<?php include (TEMPLATEPATH . '/ore/ore_template-head.php'); ?>
 <body>
 	<header class="site-header  lc">
 		<h1 class="site-logo"><a href="http://www.aleo.dk"><?php bloginfo('name'); ?></a></h1>
